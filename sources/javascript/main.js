e58c8c965c48d13e588d4013636575009bdf3b59
Bel-kun <bellaire.vivien22@gmail.com>
2022-06-11 21:27:49 -0400

    Main.js > Add html elements to main.js

---
+++ sources/javascript/main.js
@@ -0,0 +1,14 @@
+const darkness = document.querySelector('.darkness')
+const pathway = darkness.querySelector('.pathway')
+
+const floorTiles = pathway.querySelectorAll('.floor-tile')
+const floorlights = pathway.querySelectorAll('.floor-light')
+const Tiles = pathway.querySelectorAll('.tile')
+const circlelights = pathway.querySelectorAll('.circle-light')
+const circleTiles = pathway.querySelectorAll('.circle-tile')
+
+const title = pathway.querySelectorAll('.title')
+const text = pathway.querySelectorAll('.text')
+const tales = pathway.querySelectorAll('.tale')
+
+
